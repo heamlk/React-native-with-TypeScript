9cbf019fc8d5d2a720341783751a548ce8dd9b76
David <davidbalayandev@gmail.com>
2025-09-19 20:22:20 +0400

    Removed unused code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,27 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Stack } from "expo-router";
+import { View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    // <AutoLayout>
+    <View style={{ flex: 1, backgroundColor: "black" }}>
+      {/* Descope */}
+      {/* <AuthProvider projectId='P2fawBqqYZBLazNBF0lZ03UdzLQB'> */}
+      {/* Safe Area */}
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          {/* Router */}
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index"></Stack.Screen>
+          </Stack>
+          {/* Router - END */}
+        </SafeAreaView>
+      </SafeAreaProvider>
+      {/* Safe Area - END */}
+      {/* </AuthProvider> */}
+      {/* Descope - END */}
+    </View>
+    // </AutoLayout>
   );
 }
