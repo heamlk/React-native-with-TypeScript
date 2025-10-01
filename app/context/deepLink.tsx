8bfa0028f9cc03fd50e2ba2a906e08b0422dde5b
David <davidbalayandev@gmail.com>
2025-10-01 15:42:21 +0400

    Completed authentication system

--- app/context/deepLink.tsx
+++ app/context/deepLink.tsx
@@ -1,6 +1,7 @@
 import * as Linking from 'expo-linking'
 import { ReactNode, createContext, useContext, useEffect } from 'react'
 import { useAuth } from './descope'
+import { useRouter } from 'expo-router'
 
 export type DeepLinkContextType = {}
 export type DeeplinkProviderProps = { children: ReactNode }
@@ -8,19 +9,21 @@ export type DeeplinkProviderProps = { children: ReactNode }
 const DeepLinkContext = createContext<DeepLinkContextType | null>(null)
 
 export default function DeeplinkProvider({ children }: DeeplinkProviderProps) {
+  const router = useRouter()
   const auth = useAuth()
 
   useEffect(() => {
     const handleDeepLink = async ({ url }: { url: string }) => {
       const { queryParams } = Linking.parse(url)
       if (queryParams?.code) {
-        auth.oAuthCodeExchange({ code: String(queryParams?.code) })
+        const result = await auth.oAuthCodeExchange({ code: String(queryParams?.code) })
+        router.navigate('/')
       }
     }
 
     const subscription = Linking.addEventListener('url', handleDeepLink)
 
-    // Check initial URL if app is opened via deep link
+    // Checking initial URL if app is opened via deep link
     Linking.getInitialURL().then((url) => {
       if (url) handleDeepLink({ url })
     })
--- app/context/descope.tsx
+++ app/context/descope.tsx
@@ -1,23 +1,52 @@
 import * as AuthSession from 'expo-auth-session'
 import * as WebBrowser from 'expo-web-browser'
-import { createContext, ReactNode, useContext } from 'react'
+import { createContext, type Dispatch, type ReactNode, type SetStateAction, useContext, useEffect, useState } from 'react'
 import axios from 'axios'
 import * as Linking from 'expo-linking'
-import { useRouter } from 'expo-router'
+import storage from '@/app/shared/storage/storage'
 
 WebBrowser.maybeCompleteAuthSession()
 
 export type AuthContextType = {
   oAuth: ({ provider }: { provider: 'google' | 'microsoft' }) => Promise<void>
-  oAuthCodeExchange: ({ code }: { code: string }) => void
+  oAuthCodeExchange: ({ code }: { code: string }) => Promise<{ successful: boolean; data: any }>
+  otp: ({ email }: { email: string }) => Promise<{ successful: boolean; data: any }>
+  otpVerify: ({ email, code }: { email: string; code: string }) => Promise<{ successful: boolean; data: any }>
+  authenticate: ({ name, email, picture }: { name: string; email: string; picture: string }) => void
+  logout: () => void
+  user: UserType | null
+  setUser: Dispatch<SetStateAction<UserType | null>>
 }
 export type AuthProviderProps = { children: ReactNode }
 
 const AuthContext = createContext<AuthContextType | null>(null)
 
+export type UserType = {
+  name: string
+  email: string
+  picture?: string
+}
+
+const getStoredUser = (): UserType | null => {
+  const data = storage.getString('user')
+
+  try {
+    const parsedData = JSON.parse(data as any)
+    if (parsedData?.email) {
+      return parsedData
+    }
+
+    return null
+  } catch (error) {
+    console.warn(error)
+    return null
+  }
+}
+
 export default function AuthProvider({ children }: AuthProviderProps) {
-  const router = useRouter()
   const descopeProjectId = process.env.EXPO_PUBLIC_DESCOPE_PROJECT_ID!
+
+  const [user, setUser] = useState<UserType | null>(getStoredUser())
 
   const oAuth = async ({ provider }: { provider: 'google' | 'microsoft' }) => {
     axios.post(`https://api.descope.com/v1/auth/oauth/authorize?provider=${provider}&redirectUrl=${encodeURIComponent(AuthSession.makeRedirectUri({ scheme: 'bfflai' }))}`, {}, { headers: { Authorization: `Bearer ${descopeProjectId}` } }).then((res) => {
@@ -25,21 +54,92 @@ export default function AuthProvider({ children }: AuthProviderProps) {
     })
   }
 
-  const oAuthCodeExchange = ({ code }: { code: string }) => {
-    axios
-      .post('https://api.descope.com/v1/auth/oauth/exchange', { code }, { headers: { Authorization: `Bearer ${descopeProjectId}` } })
-      .then((res) => {
-        const data = res?.data
-        router.navigate('/')
-        console.log('data: ', data)
-      })
-      .catch((error) => {
-        console.log(error)
-        router.navigate('/')
-      })
+  const oAuthCodeExchange = async ({ code }: { code: string }) => {
+    try {
+      const req = await axios.post('https://api.descope.com/v1/auth/oauth/exchange', { code }, { headers: { Authorization: `Bearer ${descopeProjectId}` } })
+      const data = req?.data
+      const successful = !!data?.user?.email
+
+      if (successful) {
+        authenticate({ email: data?.user?.email, name: data?.user?.name, picture: data?.user?.picture })
+      }
+
+      return {
+        successful,
+        data: data,
+      }
+    } catch (error) {
+      console.warn(error)
+      return {
+        successful: false,
+        data: null,
+      }
+    }
   }
 
-  const value = { oAuth, oAuthCodeExchange }
+  const otp = async ({ email }: { email: string }) => {
+    try {
+      const req = await axios.post(`https://api.descope.com/v1/auth/otp/signup-in/email`, { loginId: email }, { headers: { Authorization: `Bearer ${descopeProjectId}` } })
+      const data = req?.data
+      const successful = !!data?.maskedEmail
+
+      return {
+        successful,
+        data: data,
+      }
+    } catch (error) {
+      console.warn(error)
+      return {
+        successful: false,
+        data: null,
+      }
+    }
+  }
+
+  const otpVerify = async ({ email, code }: { email: string; code: string }) => {
+    try {
+      const req = await axios.post(`https://api.descope.com/v1/auth/otp/verify/email`, { loginId: email, code }, { headers: { Authorization: `Bearer ${descopeProjectId}` } })
+      const data = req?.data
+      const successful = !!data?.user?.email
+
+      if (successful) {
+        authenticate({ email: data?.user?.email, name: data?.user?.name, picture: data?.user?.picture })
+      }
+
+      return {
+        successful,
+        data: data,
+      }
+    } catch (error) {
+      console.warn(error)
+      return {
+        successful: false,
+        data: null,
+      }
+    }
+  }
+
+  const authenticate = ({ name, email, picture }: { name: string; email: string; picture: string }) => {
+    storage.set('user', JSON.stringify({ name, email, picture }))
+    setUser({ name, email, picture })
+  }
+
+  const logout = () => {
+    setUser(null)
+    storage.delete('user')
+  }
+
+  // useEffect(() => {
+  //   console.log('user: ', user)
+  //   logout()
+  // }, [user])
+
+  // useEffect(() => {
+  //   // otp({ email: 'davidbalayandev@gmail.com' })
+  //   // otpVerify({ email: 'davidbalayandev@gmail.com', code: '' })
+  // }, [])
+
+  const value = { oAuth, oAuthCodeExchange, otp, otpVerify, authenticate, logout, user, setUser }
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
