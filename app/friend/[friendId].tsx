f546b78c8a311f0aac103625f84239f159354ab1
David <davidbalayandev@gmail.com>
2025-10-22 23:20:42 +0400

    Restricted visiting /friend page when user doesn't have a friend

--- app/friend/[friendId].tsx
+++ app/friend/[friendId].tsx
@@ -2,19 +2,29 @@ import { useLocalSearchParams, useRouter } from 'expo-router'
 import AuthenticatedLayout from '../_shared/layout/authenticatedLayout'
 import { Text, View } from '../_shared/components/reusable'
 import { useEffect, useState } from 'react'
+import { useUser } from '../_context/user'
 
 export default function User() {
   const router = useRouter()
   const params = useLocalSearchParams<{ friendId: string }>()
   const friendId = params.friendId
+  const { user, setUser, updateUser } = useUser()
 
   const [allowUser, setAllowUser] = useState(false)
 
   useEffect(() => {
-    if (friendId && friendId !== 'new') {
-      setAllowUser(true)
+    if (!friendId) {
+      router.push('/profile')
       return
     }
+
+    if (!user?.companions || user?.companions?.length === 0) {
+      router.push('/profile')
+      return
+    }
+
+    updateUser()
+    setAllowUser(true)
   }, [])
 
   if (!allowUser) {
